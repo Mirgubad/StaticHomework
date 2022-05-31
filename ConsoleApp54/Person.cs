90eaf72336b2fcb4aa1aeca4ad8b8b06301ca88a
Mirgubad <migubad@code.edu.az>
2022-05-31 16:28:14 +0400

    Proje dosyası ekle.

---
+++ ConsoleApp54/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp54
+{
+    class Person
+    {
+        private static int _age;
+        public int Age
+        {
+            get
+            {
+                return ++_age;
+            }
+            set
+            {
+            }
+
+        }
+
+    }
+}
---
+++ ConsoleApp54/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+namespace ConsoleApp54
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student tamerlan = new Student();
+            Console.WriteLine(tamerlan.Age);
+
+            Student faiq = new Student();
+            Console.WriteLine(faiq.Age);
+
+            Student tural = new Student();
+            Console.WriteLine(tural.Age);
+
+            Student emil = new Student();
+            Console.WriteLine(emil.Age);
+        }
+    }
+}
